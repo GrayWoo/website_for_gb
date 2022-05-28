ed56632151c8a80781017a7bbb123d653086cbd0
Aleksandr_OZ <korvet95@mail.ru>
2022-05-28 18:25:34 +0300

    first commit

---
+++ src/components/Main.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>Yay</main>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class Main extends Vue {}
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/consts/route.ts
@@ -0,0 +1,3 @@
+const rootUrl = "http://localhost:5555";
+
+export default rootUrl
